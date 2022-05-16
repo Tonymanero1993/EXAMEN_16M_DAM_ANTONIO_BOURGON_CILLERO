48f591925fa400af6b4488bce781361ab8e0da90
anton <anton@LAPTOP-0CG8S0EJ>
2022-05-16 09:41:53 +0200

    Rama nueva

--- src/Numero_Primo/Num_primo.java
+++ src/Numero_Primo/Num_primo.java
@@ -8,12 +8,29 @@ public class Num_primo {
 		
 		System.out.println("Buenos días Ceinmark, soy Marta Gómez");
 		
-		Scanner ent = new Scanner(System.in);
-		int num = 0;
+		Scanner ent = new Scanner(System.in);		
+		int indice = 1;
+		int r;
+		int primo = 0;
 		
 		System.out.println("Introduce un numero para averiguar si es primo o no: ");
-		num = ent.nextInt();
+		int num = ent.nextInt();
+        
+        System.out.println(num + "/" + num + " = " + num/num);
+        
+        while ((num-indice)>1) {
+            if (num%(num-indice)==0) {
+                r=num/(num-indice);
+                primo=1;
+                System.out.println(num + "/" + (num-indice) + " = " + r);
+            }
+            indice++;
+        }
 
-	}
+        System.out.println(num + "/1 = " + num);
+        
+        if (primo==0) System.out.println("El número es primo"); else System.out.println("El numero no es primo");
+        System.out.println("\n==========================");
+    }
 
 }
