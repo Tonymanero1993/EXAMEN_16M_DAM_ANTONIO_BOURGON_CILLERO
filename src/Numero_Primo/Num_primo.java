ea3b87b54b1cc259e03ac02ec2c3e0de57d34286
anton <anton@LAPTOP-0CG8S0EJ>
2022-05-16 09:22:06 +0200

    Primer Commit

---
+++ src/Numero_Primo/Num_primo.java
@@ -0,0 +1,19 @@
+package Numero_Primo;
+
+import java.util.Scanner;
+
+public class Num_primo {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Buenos días Ceinmark, soy Marta Gómez");
+		
+		Scanner ent = new Scanner(System.in);
+		int num = 0;
+		
+		System.out.println("Introduce un numero para averiguar si es primo o no: ");
+		num = ent.nextInt();
+
+	}
+
+}
